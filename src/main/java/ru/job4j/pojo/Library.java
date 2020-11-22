250823284aef36d464b87c85db7b28a55a48ae1c
PavelAll <alpa2020mi@yandex.ru>
2020-11-23 05:49:23 +0800

    3. Массивы и модели. [#393922]

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,40 @@
+package ru.job4j.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book[] books = new Book[4];
+        books[0] = new Book("Война и мир", 1274);
+        books[1] = new Book("Мастер и Маргарита", 470);
+        books[2] = new Book("Clean code", 100);
+        books[3] = new Book("Мертвые души", 300);
+
+        for (int index = 0; index < books.length; index++) {
+            Book bk = books[index];
+            System.out.println(bk.getName() + " - " + bk.getPage());
+        }
+
+        System.out.println("Swap book " + "\"" + books[0].getName() + "\"" + " with book " + "\"" + books[3].getName() + "\"");
+
+        for (int index = 0; index < books.length; index++) {
+            if (index == 0) {
+                Book temp = books[index];
+                books[index] = books[3];
+                books[3] = temp;
+            }
+        }
+
+        for (int index = 0; index < books.length; index++) {
+            Book bk = books[index];
+            System.out.println(bk.getName() + " - " + bk.getPage());
+        }
+
+        System.out.println("Output of books named \"Clean code\"");
+
+        for (int index = 0; index < books.length; index++) {
+            Book bk = books[index];
+            if (bk.getName().equals("Clean code")) {
+                System.out.println(bk.getName() + " - " + bk.getPage());
+            }
+        }
+    }
+}
