44a546620dc8be4c038bbf6429695d5cccc81799
PavelAll <alpa2020mi@yandex.ru>
2020-11-25 00:52:53 +0800

    5. Tracker - хранилище [#393930]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -5,8 +5,28 @@ import java.time.format.DateTimeFormatter;
 
 public class StartUI {
     public static void main(String[] args) {
-        Item item = new Item();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        System.out.println(item.getCreated().format(formatter));
+        Tracker tracker = new Tracker();
+        Item itemFirst = new Item();
+        Item itemSecond = new Item();
+        Item itemThird = new Item();
+
+        itemFirst.setName("Заявление 1");
+        itemSecond.setName("Заявление 2");
+        itemThird.setName("Заявление 3");
+
+        tracker.add(itemFirst);
+        tracker.add(itemSecond);
+        tracker.add(itemThird);
+
+        Item rsl[] = tracker.findByName("Заявление 2");
+
+        for (int i = 0; i < rsl.length; i++) {
+            Item itm = rsl[i];
+            System.out.println(itm.getName());
+        }
+
+        Item item = tracker.findById(3);
+        System.out.println(item.getName());
     }
 }
--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,9 +1,40 @@
 package ru.job4j.tracker;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
     private int size = 0;
+
+    public Item[] findAll() {
+        Item[] rsl = new Item[items.length];
+        int size = 0;
+        for (int i = 0; i < rsl.length; i++) {
+            Item item = items[i];
+            if (item != null) {
+                rsl[size] = item;
+                size++;
+            }
+        }
+        return Arrays.copyOf(rsl, size);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] items = findAll();
+        Item[] rsl = new Item[items.length];
+        int size = 0;
+
+        for (int i = 0; i < items.length; i++) {
+            Item item = items[i];
+            if (item.getName().equals(key)) {
+                rsl[size] = item;
+                size++;
+            }
+        }
+        return Arrays.copyOf(rsl, size);
+    }
 
     public Item add(Item item) {
         item.setId(ids++);
