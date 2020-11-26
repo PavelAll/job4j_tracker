9442a444aa1cf75ecbf0ba8e62c0aa3269676569
PavelAll <alpa2020mi@yandex.ru>
2020-11-27 05:31:11 +0800

    2. Scanner и чтение числа из консоли. [#393851]

--- src/main/java/ru/job4j/io/Matches.java
+++ src/main/java/ru/job4j/io/Matches.java
@@ -7,25 +7,22 @@ public class Matches {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         boolean run = true;
-        int matches = 11 , user = 0;
+        int matches = 11, player = 0, count = 0;
         System.out.println("Игра 11 спичек");
-        while (run) {
-            System.out.print("Первый игрок введите число от (1 до 3): ");
-            user = Integer.valueOf(input.nextLine());
-            matches -= user;
-            System.out.println("На столе осталось спичек: " + matches);
-            if (matches <= 0) {
-                System.out.println("Игрок 1 победил");
-                break;
-            }
-            System.out.print("Второй игрок введите число от (1 до 3): ");
-            user = Integer.valueOf(input.nextLine());
-            matches -= user;
-            System.out.println("На столе осталось спичек: " + matches);
-            if (matches <= 0) {
-                System.out.println("Игрок 2 победил");
-                break;
+
+        while (matches > 0) {
+            count = count == 1 ? 2 : 1;
+            System.out.print("Игрок " + count + " введите число от (1 до 3): ");
+            player = Integer.valueOf(input.nextLine());
+
+            if (player >= 1 && player <= 3) {
+                matches -= player;
+                System.out.println("На столе осталось спичек: " + matches);
+            } else {
+                System.out.println("Не верный ввод ");
+                count = count == 1 ? 2 : 1;
             }
         }
+        System.out.println("Победил игрок: " + count);
     }
 }
