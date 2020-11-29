71354e617a1bd4f411ccd9f58b3b17ae41776ff2
PavelAll <alpa2020mi@yandex.ru>
2020-11-30 07:32:45 +0800

    0.4. Пользовательские исключения. [#393872]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,27 @@
+package ru.job4j.ex;
+
+import ru.job4j.ex.ru.job4j.ex.ElementNotFoundException;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (value[i].equals(key)) {
+                rsl = i;
+            }
+        }
+        if(rsl == -1) {
+            throw new ElementNotFoundException("Element Not found exception");
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        try {
+            int index = indexOf(new String[]{"Ivan", "Vasya", "Pavel"}, "Masha" );
+            System.out.println(index);
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/ru/job4j/ex/ru/job4j/ex/ElementNotFoundException.java
@@ -0,0 +1,8 @@
+package ru.job4j.ex.ru.job4j.ex;
+
+public class ElementNotFoundException extends Exception {
+
+    public ElementNotFoundException(String message) {
+        super(message);
+    }
+}
