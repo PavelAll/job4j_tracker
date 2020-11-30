b339ffe11681007747813d4da3f03a8241094e4c
PavelAll <alpa2020mi@yandex.ru>
2020-11-30 09:50:50 +0800

    0.5. Иерархия исключений и множественный catch. [#393873]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -1,6 +1,6 @@
 package ru.job4j.ex;
 
-import java.util.Arrays;
+
 
 public class FindEl {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
@@ -16,12 +16,34 @@ public class FindEl {
         return rsl;
     }
 
-    public static void main(String[] args) {
+    public static boolean sent(String value, String[] obuses) throws ElementAbuseException {
+        for (int i = 0; i < obuses.length; i++) {
+            if (obuses[i].equals(value)) {
+                throw new ElementAbuseException("The key is included in the keys of prohibited keys");
+            }
+        }
+            return true;
+    }
+
+    public static void process(String[] value, String key, String[] obuses) {
         try {
-            int index = indexOf(new String[]{"Ivan", "Vasya", "Pavel"}, "Masha" );
-            System.out.println(index);
-        } catch (ElementNotFoundException e) {
+            if (indexOf(value, key) != -1) {
+                sent(key, obuses);
+                System.out.println("Cooбщение отправлено благополучно");
+            }
+        } catch (ElementAbuseException ea) {
+            ea.printStackTrace();
+        } catch (ElementNotFoundException en) {
+            en.printStackTrace();
+        } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public static void main(String[] args) {
+        String[] value = {"Ivan", "Maksim", "Vladimir", "Egor"};
+        String[] obuses = {"key1", "key2", "Ivan", "key4"};
+        String key = "Ivan";
+        process(value, key, obuses);
     }
 }
