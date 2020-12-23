31b252563cc5abaed432402dd9861ef1e38516e6
PavelAll <alpa2020mi@yandex.ru>
2020-12-23 11:16:47 +0800

    1. Паспорт и жители [#393838]

---
+++ src/test/java/ru/job4j/collection/hashmap/PassportOfficeTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.collection.hashmap;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class PassportOfficeTest {
+
+    @Test
+    public void whenTrue() {
+        Citizen citizen = new Citizen("2f44a", "Pavel Alekseev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        assertThat(office.get(citizen.getPassport()), is(citizen));
+    }
+
+    @Test
+    public void whenFalse() {
+        PassportOffice office = new PassportOffice();
+        Map<String, Citizen> map = new HashMap<>();
+        map.put("2f44a", new Citizen("2f44a", "Pavel Alekseev"));
+        Citizen citizen = new Citizen("2f44a", "Pavel Alekseev");
+        assertThat(office.add(citizen), is(false));
+    }
+}
