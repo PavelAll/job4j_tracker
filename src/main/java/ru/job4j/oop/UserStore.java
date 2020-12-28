ae929c800755807f9f9d5199fe673e391b643e7b
PavelAll <alpa2020mi@yandex.ru>
2020-12-28 14:26:38 +0800

    oop

--- src/main/java/ru/job4j/oop/UserStore.java
+++ src/main/java/ru/job4j/oop/UserStore.java
@@ -1,10 +1,8 @@
 package ru.job4j.oop;
 
-import ru.job4j.tracker.User;
-
 public class UserStore {
-    public static void main(String[] args) {
-        User user = new User(1, "root", "root");
-        System.out.println(user);
-    }
+//    public static void main(String[] args) {
+//        User user = new User(1, "root", "root");
+//        System.out.println(user);
+//    }
 }
