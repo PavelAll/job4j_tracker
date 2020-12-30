3f50d50de66773ac8786d48cdd5f8cdae721aabb
PavelAll <alpa2020mi@yandex.ru>
2020-12-30 12:27:07 +0800

    4. Сортировка номера [#393824]

---
+++ src/main/java/ru/job4j/collection/sortnumbers/LexSort.java
@@ -0,0 +1,22 @@
+package ru.job4j.collection.sortnumbers;
+
+import java.util.Comparator;
+
+public class LexSort implements Comparator<String> {
+    @Override
+    public int compare(String left, String right) {
+        String[]lefts = left.split("\\.");
+        String[]rights = right.split("\\.");
+
+        for (int i = 0; i < Math.min(lefts.length, rights.length); i++) {
+            try {
+                if (Integer.parseInt(lefts[i]) != Integer.parseInt(rights[i])){
+                    return Integer.compare(Integer.parseInt(lefts[i]), Integer.parseInt(rights[i]));
+                }
+            } catch (NumberFormatException e) {
+                return Integer.compare(left.length(), right.length());
+            }
+        }
+        return 0;
+    }
+}
