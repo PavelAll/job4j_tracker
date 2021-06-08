bf0933b9ae9d834db2e3913d6e3b39c353a80544
ADMIN <borisroud1@mail.ru>
2021-06-08 23:51:56 +0800

    2. Отсортировать департаменты [#34352 #165965]

--- src/main/java/ru/job4j/collection/departments/DepDescComp.java
+++ src/main/java/ru/job4j/collection/departments/DepDescComp.java
@@ -1,8 +1,6 @@
 package ru.job4j.collection.departments;
 
-import java.util.Arrays;
 import java.util.Comparator;
-import java.util.List;
 
 public class DepDescComp implements Comparator<String> {
 
@@ -10,16 +8,7 @@ public class DepDescComp implements Comparator<String> {
     public int compare(String str1, String str2) {
         String[]s1 = str1.split("/");
         String[]s2 = str2.split("/");
-        if (s2[0].compareTo(s1[0]) == 0) {
-            for (int i = 1; i < Math.min(s1.length, s2.length); i++) {
-                if (s1[i].compareTo(s2[i]) == 0) {
-                    continue;
-                }
-                return s1[i].compareTo(s2[i]);
-            }
-        } else {
-            return s2[0].compareTo(s1[0]);
-        }
-        return Integer.compare(str1.length(), str2.length());
+        int rsl = s2[0].compareTo(s1[0]);
+        return rsl == 0 ? str1.compareTo(str2) : rsl;
     }
 }
