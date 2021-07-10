1516cceb68e0400be4f8a39d06f42789f841c084
PavelAll <alpa2020mi@yandex.ru>
2021-07-10 17:21:40 +0800

    8. Перенос методов в Stream API [#279131]

---
+++ src/main/java/ru/job4j/streams/deck/DeckOfCards.java
@@ -0,0 +1,42 @@
+package ru.job4j.streams.deck;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+enum Suit {
+    Diamonds, Hearts, Spades, Clubs
+}
+
+enum Value {
+    V_6, V_7, V_8
+}
+
+class Card {
+    private Suit suit;
+    private Value value;
+
+    public Card(Suit suit, Value value) {
+        this.suit = suit;
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Card{"
+                + "suit=" + suit
+                + ", value=" + value + '}';
+    }
+}
+
+public class DeckOfCards {
+    public static void main(String[] args) {
+        List<Card> desk = Stream.of(Value.values())
+                .flatMap(value -> Stream.of(Suit.values())
+                        .map(suit -> new Card(suit, value)))
+                .collect(Collectors.toList());
+        desk.forEach(System.out::println);
+    }
+}
